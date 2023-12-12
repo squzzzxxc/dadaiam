ff6a64baf21f812ed8b0b2d47cf28780a8235fc1
Egor <koshekot1215@gmail.com>
2023-12-12 10:59:18 +0300

    first commit

---
+++ WpfApp2/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class DBContext
+    {
+        private static Entities _entities;
+
+        public static Entities GetContext()
+        {
+            if (_entities == null)
+            {
+                _entities = new Entities();
+            }
+            return _entities;
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenderComboBox.ItemsSource = DBContext.GetContext().IMP_UP07_Gender.ToList();
+            UpdateClients();
+        }
+        private void UpdateClients()
+        {
+            var clients = DBContext.GetContext().IMP_UP07_Client.ToList();
+            if (GenderComboBox.SelectedIndex != -1)
+            {
+                IMP_UP07_Gender gender = GenderComboBox.SelectedItem as IMP_UP07_Gender;
+                clients = clients.Where(p => p.GenderId == gender.Id).ToList();
+            }
+            if(!string.IsNullOrEmpty(SearchTextBox.Text))
+                clients = clients.Where(p=>p.Name.Contains(SearchTextBox.Text) || p.LastName.Contains(SearchTextBox.Text)).ToList();
+            if(SortComboBox.SelectedIndex != -1)
+            {
+                switch(SortComboBox.SelectedIndex)
+                {
+                    case 0:
+                        clients = clients.OrderBy(p => p.Name).ToList();
+                        break;
+                    case 1:
+                        clients = clients.OrderByDescending(p => p.Name).ToList();
+                        break;
+                    case 2:
+                        clients = clients.OrderBy(p => p.LastName).ToList();
+                        break;
+                    case 3:
+                        clients = clients.OrderByDescending(p => p.LastName).ToList();
+                        break;
+                }
+            }
+            if(clients.Count == 0)
+            {
+                MessageBox.Show("Ничего не найдено", "Результаты поиска");
+            }
+            dataGrid.ItemsSource = clients;
+        }
+
+        private void GenderComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateClients();
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateClients();
+        }
+    }
+}
